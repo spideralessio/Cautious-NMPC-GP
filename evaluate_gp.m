bd09e691826be1ec83a869e8cbf0fd11c5a2b3c0
dylansavoia <dylansavoia@gmail.com>
2021-03-11 10:54:22 +0100

    Online Learning + Plots

--- evaluate_gp.m
+++ evaluate_gp.m
@@ -1,6 +1,12 @@
 function [ypred] = evaluate_gp(x,u)
-    load('gp_model.mat');
-    ypred = [x',u']*w + b;
-    ypred = ypred';
+    if exist('lr_online_model.mat', 'file')
+        load('lr_online_model.mat'); % or gp_model.mat
+        ypred = [x',u']*w + b;
+        ypred = ypred';
+    else
+        ypred = zeros(3, 1);
+    end
+    
+    ypred
 end
 
--- evaluate_gp_deriv.m
+++ evaluate_gp_deriv.m
@@ -1,5 +1,9 @@
-function [ypred_prime] = evaluate_gp(x,u)
-    load('gp_model.mat');
-    ypred_prime = w';
+function [ypred_prime] = evaluate_gp_deriv(x,u)
+    if exist('lr_online_model.mat', 'file')
+        load('lr_online_model.mat'); % or gp_model.mat
+        ypred_prime = w';
+    else
+        ypred_prime = zeros(3, 10);
+    end
 end
 
